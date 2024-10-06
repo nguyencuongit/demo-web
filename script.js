b9183b81d2fe51b2f8093a51a1e0cc04bd976db2
Nguyen It <nguyenhuycuong174@gmail.com>
2024-10-06 15:17:10 +0700

    deco web

---
+++ script.js
@@ -0,0 +1,4 @@
+const adressclose = document.querySelector('.background-close')
+adressclose.addEventListener("click", function(){
+    document.querySelector('.background').style.display = "none"
+})
